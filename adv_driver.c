2ce76a7701cdca02e374d570044b92fe1e732e91
Rahul Singh Rawat <rahulsrawat42@gmail.com>
2019-08-23 13:40:43 +0530

    module param added dynamic debug enable/disable from syfs added

--- adv_driver.c
+++ adv_driver.c
@@ -1,6 +1,41 @@
 #include<linux/kernel.h>
 #include<linux/init.h>
 #include<linux/module.h>
+#include<linux/moduleparam.h>
+int node, cb_debug_val=0;
+char *cb_debug;
+module_param(node, int, S_IRUSR|S_IWUSR);
+module_param(cb_debug_val, int, S_IRUSR|S_IWUSR);
+/*----------------------Module_param_cb()--------------------------------*/
+int change_debug_lvl(const char *val, const struct kernel_param *kp)
+{
+        int res = param_set_int(val, kp); // Use helper for write variable
+        if(res==0) {
+                printk(KERN_INFO "Call back function called...\n");
+		switch (cb_debug_val) {
+			case 0:
+				#ifdef DEBUG
+				#undef DEBUG
+				#endif
+				break;
+			case 1:
+				#ifndef DEBUG
+				#define DEBUG
+				#endif
+				break;
+			}
+                return 0;
+        }
+        return -1;
+}
+
+const struct kernel_param_ops my_param_ops = 
+{
+        .set = &change_debug_lvl, // Use our setter ...
+        .get = &param_get_int, // .. and standard getter
+};
+
+module_param(cb_debug, charp, S_IRUSR|S_IWUSR);
 
 static int __init first_function(void)
 {
